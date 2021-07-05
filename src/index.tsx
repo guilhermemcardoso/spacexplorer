fcbff2eb61cd030682e2d747d99076562b5df136
Guilherme Cardoso <guilherme@Guilhermes-MacBook-Air.local>
2021-07-04 23:12:00 -0300

    configured splash screen

--- src/index.tsx
+++ src/index.tsx
@@ -1,9 +1,14 @@
 import 'react-native-gesture-handler';
-import React from 'react';
+import React, {useEffect} from 'react';
 import Router from './router';
 import {LaunchProvider} from './contexts/launchContext';
+import SplashScreen from 'react-native-splash-screen';
 
 const App = () => {
+  useEffect(() => {
+    SplashScreen.hide();
+  }, []);
+
   return (
     <LaunchProvider>
       <Router />
